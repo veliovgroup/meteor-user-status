9693ddf662c46ca7d7c094614c2b4d7205d1a8b8
dr.dimitru <ceo@veliov.com>
2017-05-16 22:27:00 +0300

    v1.0.0
     - Compatibility with `meteor@1.4.4.2`
     - No more CoffeeScript
     - Move codebase to ES6
     - __Breaking:__ `UserStatus` is not exposed to global scope, use ES6
    `include`

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:user-status',
-  version: '0.6.0',
+  version: '1.0.0',
   summary: 'Reactively check user\'s [on|off]line and idle status',
   git: 'https://github.com/VeliovGroup/Meteor-user-status',
   documentation: 'README.md'
@@ -8,10 +8,9 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
-  api.mainModule('client/main.coffee', 'client');
-  api.addFiles('server/main.coffee', 'server');
-  api.use(['coffeescript', 'accounts-base', 'accounts-password', 'ecmascript'], ['client', 'server']);
+  api.mainModule('client/main.js', 'client');
+  api.addFiles('server/main.js', 'server');
+  api.use(['accounts-base', 'accounts-password', 'ecmascript'], ['client', 'server']);
   api.use('random', 'server');
-  api.use(['session', 'reactive-var', 'underscore', 'random', 'tracker'], 'client')
-  api.export('UserStatus');
+  api.use(['session', 'reactive-var', 'underscore', 'tracker'], 'client');
 });
